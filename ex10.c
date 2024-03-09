ccb1aaa9f4c89823e0c1195e2f3ced956667591a
Leandro Lopes <153689451+Leandroslopez@users.noreply.github.com>
2024-03-08 21:35:01 -0400

    atualização ex10

--- ex10.c
+++ ex10.c
@@ -2,23 +2,24 @@
 #include<stdlib.h>
 int main() {
 
-    float sal_b, sal_imp, sal_reb, grat, impos;
+    float sal_b, grat, imp, n_sal;
 
     sal_b = 0;
-    sal_reb = 0;
-    sal_imp = 0;
     grat = 0;
-    impos = 0;
+    imp = 0;
+    n_sal = 0;
 
-    printf("Calcular salario final do funcionario depois das gratificacoes e impostos\n");
+    printf("Calcular salario final do funcionario depois das gratificacoes e impostos.\n");
     printf("Informe o salario base do funcionario: R$ ");
     scanf("%f", &sal_b);
 
     grat = sal_b * 0.05;
-    sal_imp = sal_b + grat;
-    impos = sal_imp * 0.07;
+    imp = (sal_b + grat) * 0.07;
+    n_sal = (sal_b + grat) - imp;
     
-
+    printf("A gratificacao do funcionario foi de R$ %.2f \n", grat);
+    printf("O imposto pago pelo funcionario foi de R$ %.2f\n", imp);
+    printf("O salario do funcionario sera de R$ %.2f", n_sal);
 
 
 
